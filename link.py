f63f5e37fd5830df375b3fd258e838076162e850
Victor Soupday <79094830+soupday@users.noreply.github.com>
2026-07-15 21:25:36 +0100

    2.4.2
    -

--- link.py
+++ link.py
@@ -3931,8 +3931,8 @@ class LinkService():
             self.update_sequence(5, delta_frames)
 
     def get_remote_file(self, remote_id: str, source_path: str, file_override=None):
-        if os.pathsep != "\\":
-            source_path = source_path.replace("\\", os.pathsep)
+        if os.path.sep != "\\":
+            source_path = source_path.replace("\\", os.path.sep)
         print(f"remote_id={remote_id}, source_path={source_path}, file_override={file_override}")
         # remote_id=1784145927532161400,
         # source_path=C:\Users\t3xr9\AppData\Local\Temp\CharacterCreator5Temp\Blender DataLink\exports\Camila\Camila.fbx,
